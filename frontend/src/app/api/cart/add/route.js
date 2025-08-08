ef294c479965dd86bcf7914e8c6ad21e231c6c3a
EZIO AUDITORE <ezioauditoredefinerz@gmail.com>
2025-08-08 07:59:52 +0530

    almost final

--- frontend/src/app/api/cart/add/route.js
+++ frontend/src/app/api/cart/add/route.js
@@ -3,7 +3,7 @@ import { addToCart } from '../../../db/cartDb';
 
 export async function POST(req) {
   try {
-    const { username, item } = await req.json();
+    const { username, item,price } = await req.json();
 console.log('Received data:', { username, item ,price});
   
     addToCart(username, item,price);
